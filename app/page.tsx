a20f524ebfed7420ef3299b1d3a2ab194b133196
AobaIwaki <aobaiwaki@gmail.com>
2025-03-12 01:43:00 +0900

    add: some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import { Group } from '@mantine/core';
+import { ButtonProgress } from '../components/ButtonProgress/ButtonProgress';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { FooterSocial } from '../components/FooterSocial/FooterSocial';
+import { GridAsymmetrical } from '../components/GridAsymmetrical/GridAsymmetrical';
+import { HeaderSimple } from '../components/HeaderSimple/HeaderSimple';
 import { Welcome } from '../components/Welcome/Welcome';
 
+
 export default function HomePage() {
+
   return (
     <>
+      <HeaderSimple />
       <Welcome />
-      <ColorSchemeToggle />
+      <Group justify="center" mt="xl">
+        <ButtonProgress />
+      </Group>
+      <GridAsymmetrical />
+      <FooterSocial />
     </>
   );
 }
---
+++ components/ButtonProgress/ButtonProgress.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState } from 'react';
+import { Button, Group, Progress, rgba, useMantineTheme } from '@mantine/core';
+import { useInterval } from '@mantine/hooks';
+import classes from './ButtonProgress.module.css';
+
+export function ButtonProgress() {
+  const theme = useMantineTheme();
+  const [progress, setProgress] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const interval = useInterval(
+    () =>
+      setProgress((current) => {
+        if (current < 100) {
+          return current + 1;
+        }
+
+        interval.stop();
+        setLoaded(true);
+        return 0;
+      }),
+    20
+  );
+
+  return (
+    <Button
+      className={classes.button}
+      onClick={() => (loaded ? setLoaded(false) : !interval.active && interval.start())}
+      color={loaded ? 'teal' : theme.primaryColor}
+    >
+      <div className={classes.label}>
+        {progress !== 0 ? 'Processing...' : loaded ? 'Process Finished!' : 'Add Music!'}
+      </div>
+      {progress !== 0 && (
+        <Progress
+          value={progress}
+          className={classes.progress}
+          color={rgba(theme.colors.blue[2], 0.35)}
+          radius="sm"
+        />
+      )}
+    </Button>
+  );
+}
